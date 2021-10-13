3a5c134995629074ee9dd72bb4cecaf73c97a42a
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2021-10-13 13:59:02 +0200

    modificación

--- src/com/dam/paquete1/ejercicio1/Ejercicio1.java
+++ src/com/dam/paquete1/ejercicio1/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.dam.paquete1.ejercicio1;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("hola1");
+        System.out.println("modificación");
     }
 }
